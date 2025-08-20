2cfb1b8ee3525d8b92078b6e18d5a9f9565aa41b
shohjahon <xayrullayevshohjahon871@gmail.com>
2025-08-20 18:25:17 +0500

    Add files via upload

---
+++ socript.js
@@ -0,0 +1,25 @@
+let inson = {
+  ism: "Ali",
+  familiya: "Valiyev",
+  yosh: 10,
+};
+
+console.log(inson.ism);
+console.log(inson["ism"]);
+
+inson.manzil = "Toshkent";
+console.log(inson);
+
+let car = {};
+
+let model = prompt("Modelni kiriting");
+
+let yil = prompt("Yilni kiriting");
+
+car.modeli = model;
+car.yili = yil;
+
+console.log(car);
+
+
+
